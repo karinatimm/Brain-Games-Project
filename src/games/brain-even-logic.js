1e03492b6b84cc0339f28b407d54e5c360f17d65
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-10-18 15:47:59 +0200

    Added src/games/brain-even-logic.js

---
+++ src/games/brain-even-logic.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { welcomeUser, gameEngine } from '../index.js';
+
+export default function isEven() {
+  const userName = welcomeUser();
+
+  const gameInstructions = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+  function gameLogic() {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+    const randomQuestion = `${randomNumber}`;
+    const correctAnswer = `${randomNumber % 2 === 0 ? 'yes' : 'no'}`;
+
+    return [randomQuestion, correctAnswer];
+  }
+
+  gameEngine(gameLogic, gameInstructions, userName);
+}
