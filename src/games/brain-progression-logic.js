6081b3ef097f648ff52e3911ea2502fd2d029767
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-10-20 16:49:37 +0200

    Updated src/games/brain-progression-logic.js after correction2

--- src/games/brain-progression-logic.js
+++ src/games/brain-progression-logic.js
@@ -1,11 +1,11 @@
 import { runGameEngine, generateRandomNumber } from '../index.js';
 
-function generateProgression(
+const generateProgression = (
   progrLength,
   indexOfHiddenNum,
   firstNumInProgr,
   differenceInProgr,
-) {
+) => {
   const generatedProgression = [];
 
   for (let i = 0; i < progrLength; i += 1) {
@@ -15,39 +15,41 @@ function generateProgression(
       generatedProgression.push(firstNumInProgr + i * differenceInProgr);
     }
   }
-  return generatedProgression.join(' ');
-}
+  return generatedProgression;
+};
 
-export default function findMissingNumInProgressionGame() {
-  const gameInstructions = 'What number is missing in the progression?';
+const generateGameLogic = () => {
+  const minLength = 5;
+  const maxLength = 10;
+  const progrLength = (generateRandomNumber() % (maxLength - minLength + 1)) + minLength;
 
-  function generateGameLogic() {
-    const minLength = 5;
-    const maxLength = 10;
-    const progrLength = Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
+  // between 0 and 100(inclusive)
+  const firstNumInProgr = generateRandomNumber();
 
-    // between 0 and 100(inclusive)
-    const firstNumInProgr = generateRandomNumber();
+  // between 1 and 10(inclusive)
+  const differenceInProgr = (generateRandomNumber() % 10) + 1;
 
-    // between 1 and 10(inclusive)
-    const differenceInProgr = Math.floor(Math.random() * 10) + 1;
+  // between 1 and progrLength(excluded)
+  const indexOfHiddenNum = (generateRandomNumber() % (progrLength - 1)) + 1;
 
-    // between 1 and progrLength(excluded)
-    const indexOfHiddenNum = Math.floor(Math.random() * progrLength);
+  const randomQuestion = generateProgression(
+    progrLength,
+    indexOfHiddenNum,
+    firstNumInProgr,
+    differenceInProgr,
+  ).join(' ');
 
-    const randomQuestion = generateProgression(
-      progrLength,
-      indexOfHiddenNum,
-      firstNumInProgr,
-      differenceInProgr,
-    );
+  const correctAnswer = `${
+    firstNumInProgr + indexOfHiddenNum * differenceInProgr
+  }`;
 
-    const correctAnswer = `${
-      firstNumInProgr + indexOfHiddenNum * differenceInProgr
-    }`;
+  return [randomQuestion, correctAnswer];
+};
 
-    return [randomQuestion, correctAnswer];
-  }
+const runFindMissingNumInProgressionGame = () => {
+  const gameInstruction = 'What number is missing in the progression?';
 
-  runGameEngine(generateGameLogic, gameInstructions);
-}
+  runGameEngine(generateGameLogic, gameInstruction);
+};
+
+export default runFindMissingNumInProgressionGame;
