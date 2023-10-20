4d93af9bb6f972691c58fea80ca49356099d57e1
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-10-20 12:49:16 +0200

    Added bin/brain-prime.js after correction

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import isPrimeNumber from '../src/games/brain-prime-logic.js';
+import isPrimeNumberGame from '../src/games/brain-prime-logic.js';
 
-isPrimeNumber();
+isPrimeNumberGame();
