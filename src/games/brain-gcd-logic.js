5773487e21638b9d469576105c7ae575ec971985
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-10-20 16:51:02 +0200

    Updated src/games/brain-gcd-logic.js after correction2

--- src/games/brain-gcd-logic.js
+++ src/games/brain-gcd-logic.js
@@ -1,27 +1,29 @@
 import { runGameEngine, generateRandomNumber } from '../index.js';
 
-export default function findGreatestCommonDivisorGame() {
-  const gameInstructions = 'Find the greatest common divisor of given numbers.';
-
-  function calculateGCD(num1, num2) {
-    let a = num1;
-    let b = num2;
-    while (b !== 0) {
-      const temp = a;
-      a = b;
-      b = temp % b;
-    }
-    return a;
+const calculateGCD = (num1, num2) => {
+  let a = num1;
+  let b = num2;
+  while (b !== 0) {
+    const temp = a;
+    a = b;
+    b = temp % b;
   }
+  return a;
+};
 
-  function generateGameLogic() {
-    const randomNumber1 = generateRandomNumber();
-    const randomNumber2 = generateRandomNumber();
-    const correctAnswer = calculateGCD(randomNumber1, randomNumber2).toString();
+const generateGameLogic = () => {
+  const randomNumber1 = generateRandomNumber();
+  const randomNumber2 = generateRandomNumber();
+  const correctAnswer = calculateGCD(randomNumber1, randomNumber2).toString();
 
-    const randomQuestion = `${randomNumber1} ${randomNumber2}`;
-    return [randomQuestion, correctAnswer];
-  }
+  const randomQuestion = `${randomNumber1} ${randomNumber2}`;
+  return [randomQuestion, correctAnswer];
+};
 
-  runGameEngine(generateGameLogic, gameInstructions);
-}
+const runFindGreatestCommonDivisorGame = () => {
+  const gameInstruction = 'Find the greatest common divisor of given numbers.';
+
+  runGameEngine(generateGameLogic, gameInstruction);
+};
+
+export default runFindGreatestCommonDivisorGame;
