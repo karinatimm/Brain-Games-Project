eb2ef252321d0eabf0659b91ca95a0f747484762
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-10-20 12:51:13 +0200

    Added bin/brain-calc.js after correction

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import calc from '../src/games/brain-calc-logic.js';
+import calculateMathExpressionGame from '../src/games/brain-calc-logic.js';
 
-calc();
+calculateMathExpressionGame();
