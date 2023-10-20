bf3113850787ac001160c3b43e25d5e91e8bc43a
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-10-20 16:54:32 +0200

    Updated bin/brain-even.js after correction2

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import isEvenNumberGame from '../src/games/brain-even-logic.js';
+import runIsEvenNumberGame from '../src/games/brain-even-logic.js';
 
-isEvenNumberGame();
+runIsEvenNumberGame();
