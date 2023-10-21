4643fa7d4e2422670b5b2c827cc12e44db387c51
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-10-21 17:08:11 +0200

    Finished src/games/brain-even-logic.js

--- src/games/brain-even-logic.js
+++ src/games/brain-even-logic.js
@@ -3,7 +3,7 @@ import { runGameEngine, generateRandomNumber } from '../index.js';
 const isNumberEven = (num) => num % 2 === 0;
 
 const generateGameLogic = () => {
-  const randomNumber = generateRandomNumber(0, 100);
+  const randomNumber = generateRandomNumber();
   const correctAnswer = isNumberEven(randomNumber) ? 'yes' : 'no';
 
   return [randomNumber, correctAnswer];
