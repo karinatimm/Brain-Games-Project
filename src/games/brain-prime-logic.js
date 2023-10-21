05a1266dc61f7c91e9b3c0a10771214359487b25
Karina Timofeeva <kar.timofeeva93@gmail.com>
2023-10-21 17:07:07 +0200

    Finished src/games/brain-prime-logic.js

--- src/games/brain-prime-logic.js
+++ src/games/brain-prime-logic.js
@@ -13,7 +13,7 @@ const isNumberPrime = (num) => {
 };
 
 const generateGameLogic = () => {
-  const randomNumber = generateRandomNumber(0, 100);
+  const randomNumber = generateRandomNumber();
 
   const correctAnswer = isNumberPrime(randomNumber) ? 'yes' : 'no';
 
